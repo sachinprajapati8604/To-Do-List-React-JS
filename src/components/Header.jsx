b96f705cff56e1ebcee898876f5950aba6e555a1
Sachin Prajapati <53299703+sachinprajapati8604@users.noreply.github.com>
2021-08-22 22:37:48 +0530

    Updated

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+// import logo from "../assets/images/logo.jpg";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <nav>
+        <div className="logo heading">
+         My To-Dos
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
